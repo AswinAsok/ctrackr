74c1dc4b4584e60e3a98fe4f25f830d7981e83cb
Aswin <aswinasokofficial@gmail.com>
2024-04-22 10:15:10 +0530

    fix(navbar):bug

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,39 +3,40 @@ import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [userObject, setUserObject] = useState<any>();
-    useEffect(() => {
-        setUserObject(localStorage.getItem("userObject")!);
-        if (userObject != "") {
-            setIsAuthenticated(true);
-            console.log(userObject);
-        }
-    }, []);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userObject, setUserObject] = useState<any>();
+  useEffect(() => {
+    setUserObject(localStorage.getItem("userObject")!);
+    if (userObject != "") {
+      setIsAuthenticated(true);
+      console.log(userObject);
+    }
+  }, []);
 
-    return (
-        <>
-            <nav className={styles.navbarContainer}>
-                <p className={styles.navbarHeaderText}>CTrackr</p>
-                {isAuthenticated && userObject != "" && userObject.email ? (
-                    <>
-                        <div className={styles.row}>
-                            <p className={styles.welcomeText}>
-                                Hi, {userObject.email.substring(0, userObject.email.indexOf("@"))}
-                            </p>
-                            <Link to="/">
-                                <button className={styles.signUpButton}>Logout</button>
-                            </Link>
-                        </div>
-                    </>
-                ) : (
-                    <Link to="/signup">
-                        <button className={styles.signUpButton}>SignUp Now</button>
-                    </Link>
-                )}
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav className={styles.navbarContainer}>
+        <p className={styles.navbarHeaderText}>CTrackr</p>
+        {isAuthenticated && userObject != "" && userObject?.email ? (
+          <>
+            <div className={styles.row}>
+              <p className={styles.welcomeText}>
+                Hi,{" "}
+                {userObject?.email.substring(0, userObject?.email.indexOf("@"))}
+              </p>
+              <Link to="/">
+                <button className={styles.signUpButton}>Logout</button>
+              </Link>
+            </div>
+          </>
+        ) : (
+          <Link to="/signup">
+            <button className={styles.signUpButton}>SignUp Now</button>
+          </Link>
+        )}
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
